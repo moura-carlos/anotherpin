86611395c1ab5002d6a68b2a5c6bcdf711e01c56
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-06-13 14:35:36 -0300

    Some edits

--- db/migrate/20220605014116_create_order_items.rb
+++ db/migrate/20220605014116_create_order_items.rb
@@ -2,8 +2,8 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
       t.integer :quantity
-      t.references :order, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
+      t.references :order, null: true, foreign_key: true
+      t.references :product, null: true, foreign_key: true
 
       t.timestamps
     end
